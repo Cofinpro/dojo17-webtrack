157106eb19e8e1011a9c59af4423f7bd07adb51f
Rafael <rafael_baur@web.de>
2017-09-08 16:08:58 +0200

    modified player.ts

---
+++ frontend/src/app/models/player.ts
@@ -0,0 +1,19 @@
+/**
+ * The Player class
+ * - represents a player on the tileset field
+ * - can be controlled by the user
+ * - is synchronized to other users
+ *
+ */
+export class Player {
+
+
+    id : String;
+    x:number;
+    y:number;
+
+    constructor(){}
+
+
+
+}
