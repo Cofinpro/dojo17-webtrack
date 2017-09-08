05e88a92db38c1626ba4fda69e11eb48d0b53e80
Fabian Adler <fabian.adler@cofinpro.de>
2017-09-08 16:16:00 +0200

    IMPL * added websocket service and test sender and receiver in app module

--- frontend/src/app/models/message.ts
+++ frontend/src/app/models/message.ts
@@ -3,4 +3,9 @@ export class Message {
     constructor(public text: string) {
     }
 
+    static factory(raw: any): Message {
+        const message: Message = new Message(raw.text);
+        return message;
+    }
+
 }
