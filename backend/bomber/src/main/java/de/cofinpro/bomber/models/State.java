0530199948a71f85bb2192ae9942c40bb14bd60f
Kevin Kessenich <kevin.kessenich@cofinpro.de>
2017-09-08 17:37:24 +0200

    game logic

---
+++ backend/bomber/src/main/java/de/cofinpro/bomber/models/State.java
@@ -0,0 +1,35 @@
+package de.cofinpro.bomber.models;
+
+import java.util.Date;
+import java.util.List;
+
+public class State {
+
+    private List<Player> players;
+    private List<Bomb> bombs;
+    private Date serverTime;
+
+    public List<Player> getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(List<Player> players) {
+        this.players = players;
+    }
+
+    public List<Bomb> getBombs() {
+        return bombs;
+    }
+
+    public void setBombs(List<Bomb> bombs) {
+        this.bombs = bombs;
+    }
+
+    public Date getServerTime() {
+        return serverTime;
+    }
+
+    public void setServerTime(Date serverTime) {
+        this.serverTime = serverTime;
+    }
+}
