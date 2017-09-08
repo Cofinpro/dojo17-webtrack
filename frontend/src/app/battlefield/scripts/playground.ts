3ddb6059f0f406f4bf5fefd5fb8000034bca9709
Thomas Krämer <thomas.kraemer@cofinpro.de>
2017-09-08 16:48:34 +0200

    added hardcoded layBomb

--- frontend/src/app/battlefield/scripts/playground.ts
+++ frontend/src/app/battlefield/scripts/playground.ts
@@ -1,5 +1,6 @@
 import { GEPicture } from "./gameelements/gepicture";
 import { GameNotification } from './messages/gamenotification';
+import {Bomb} from "../../models/bomb";
 
 export class PlayGround {
     image: any;
@@ -55,6 +56,10 @@ export class PlayGround {
         this.tag.style.width = width + 'px';
         this.tag.style.height = height + 'px';
 
+    }
+
+    public layBomb(): Bomb {
+      return new Bomb('killerqueen', 5,5,'superme', null);
     }
 
     public setPositionStyle(positionStyle): void {
