97f0ca8fe44c2e7e3abb447a5e774eaf47c62caa
Marcos Scholtz <marcos.scholtz@cofinpro.de>
2017-09-08 14:55:20 +0200

    broken

--- backend/python/server.py
+++ backend/python/server.py
@@ -4,25 +4,22 @@ import asyncio
 import websockets
 import pika
 
-connection = pika.BlockingConnection(pika.ConnectionParameters('rabbit'))
-channel = connection.channel()
-
-channel.queue_declare(queue='hello')
-
-channel.basic_publish(exchange='',
-                      routing_key='hello',
-                      body='Hello World!')
-print(" [x] Sent 'Hello World!'")
-
-connection.close()
-
 async def hello(websocket, path):
-    name = await websocket.recv()
-    print("< {}".format(name))
+    # TODO: Subscribe to an exchange thing
 
-    greeting = "Hello {}!".format(name)
-    await websocket.send(greeting)
-    print("> {}".format(greeting))
+    # TODO: while alive?
+        # TODO: receive a JSON - what did he do
+        name = await websocket.recv()
+
+        # TODO: send message to a queue with "what did he do"
+        print("< {}".format(name))
+
+        greeting = "Hello {}!".format(name)
+        await websocket.send(greeting)
+        print("> {}".format(greeting))
+
+    # TODO: Unsubscribe?
+
 
 print('start server')
 start_server = websockets.serve(hello, '0.0.0.0', 3000)
