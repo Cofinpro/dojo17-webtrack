e8e7642e1a7e31befda73d353d3c616ad61d1180
Kevin Kessenich <kevin.kessenich@cofinpro.de>
2017-09-08 11:54:17 +0200

    websocket test client

--- backend/python/server.py
+++ backend/python/server.py
@@ -11,7 +11,7 @@ async def hello(websocket, path):
     await websocket.send(greeting)
     print("> {}".format(greeting))
 
-start_server = websockets.serve(hello, 'localhost', 3000)
+start_server = websockets.serve(hello, '127.0.0.1', 3000)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
