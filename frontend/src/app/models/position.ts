7c571c20856084d050bc0fed99d62cd66ef4c5c1
Rafael <rafael_baur@web.de>
2017-09-09 10:41:54 +0200

    modified position

--- frontend/src/app/models/position.ts
+++ frontend/src/app/models/position.ts
@@ -1,2 +1,16 @@
-export class position {
+export class Position {
+
+
+    x:number;
+    y:number;
+    
+
+    constructor(obj = {} as Position) {
+        this.x = obj.x;
+        this.y = obj.y;
+
+      }
+
+
 }
+
