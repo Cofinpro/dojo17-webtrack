eb7617ae3e7365e8e2cffa7e6a789ad4e62ef488
Thomas Krämer <thomas.kraemer@cofinpro.de>
2017-09-09 13:21:06 +0200

    removed playerdata service

--- frontend/src/app/battlefield/battlefield.component.ts
+++ frontend/src/app/battlefield/battlefield.component.ts
@@ -2,13 +2,12 @@ import { WebsocketService } from '../services/websocket.service';
 import { Component, OnInit, OnDestroy, HostListener } from '@angular/core';
 
 import { Game } from './scripts/game/game';
-import {PlayerDataService} from "../services/player-data.service";
 
 @Component({
     selector: 'battlefield',
     templateUrl: './battlefield.component.html',
     styleUrls: ['./battlefield.component.scss'],
-    providers: [WebsocketService, PlayerDataService]
+    providers: [WebsocketService]
 })
 
 /**
