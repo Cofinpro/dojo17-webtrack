78cf39bcaede2f45f5d301d21cced5477c29c1e8
Rafael <rafael_baur@web.de>
2017-09-09 10:26:15 +0200

    add position and modified state:x

--- frontend/src/app/models/state.ts
+++ frontend/src/app/models/state.ts
@@ -2,20 +2,20 @@ import { Bomb } from "./bomb";
 import { Player } from "./player";
 
 export class State {
+    sizeX : number;
+    sizeY : number;
     players: Player[];
     bombs: Bomb[];
     timestamp: Date;
 
-    static readonly mock: State = new State({ 
-        players: [new Player({ id: "player-one", x: 1, y: 1, nickName: "GodPlayer" })],
-        bombs: [new Bomb({ id: "a-bomb", x: 4, y: 4, userId: "player-one", detonateAt: new Date() })], 
-        timestamp: new Date()
-    });
+
 
     constructor(obj = {} as State) {
         this.players = obj.players;
         this.bombs = obj.bombs;
         this.timestamp = obj.timestamp;
+        this.sizeX = obj.sizeX;
+        this.sizeY = obj.sizeY;
     }
 
     static getMock(x: number): State {
@@ -23,7 +23,9 @@ export class State {
         return new State({ 
             players: [new Player({ id: "player-one", x: (x % 15) + 1, y: 1, nickName: "GodPlayer" })],
             bombs: [new Bomb({ id: "a-bomb", x: 3, y: 3, userId: "player-one", detonateAt: new Date(now + (5 - (x % 6)) * 1000) })],
-            timestamp: new Date(now)
+            timestamp: new Date(now),
+            sizeX : 2,
+            sizeY : 3
         });
     }
 
