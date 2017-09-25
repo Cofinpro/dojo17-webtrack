48ae13635a2ecf12227ed3dc95f425356d5a37d2
marcus hinz <marcus.fj.hinz@gmail.com>
2017-09-25 08:03:35 +0200

    IMPLEMENTED * add index.ts

---
+++ frontend/src/app/paint/index.ts
@@ -0,0 +1,5 @@
+import { PaintableCanvas } from './paintable-canvas';
+import { PositionedPaintableCanvas } from './positioned-paintable-canvas';
+import { PaintedCanvas } from './painted-canvas';
+
+export { PaintableCanvas, PositionedPaintableCanvas, PaintedCanvas};
