a0d8e55ec03760dd498eb7c6cbdc7bcfbbf1522c
marcus hinz <marcus.fj.hinz@gmail.com>
2017-09-25 08:07:47 +0200

    FIXED * code cleanup

--- frontend/src/app/paint/painted-canvas.ts
+++ frontend/src/app/paint/painted-canvas.ts
@@ -1,51 +1,19 @@
 import { PaintableCanvas } from './paintable-canvas';
 
 export class PaintedCanvas {
-    id;
-    top : number;
-    left : number;
-    width: number;
-    height: number;
-    right : number = this.left + this.width;
-    bottom : number = this.top + this.height;
 
-    constructor(id, elmTop : number, elmLeft : number, public paintableCanvas : PaintableCanvas, private context: any) {
-
-        this.id = id;
-        this.context = context;
-
-
-        this.top = elmTop;
-        this.left = elmLeft;
-        this.height = paintableCanvas.height;
-        this.width = paintableCanvas.width;
-        this.right = this.left + this.width;
-        this.bottom = this.top + this.height;
-
+    constructor(private id, private elmLeft : number, private elmTop : number, private paintableCanvas : PaintableCanvas, private context: CanvasRenderingContext2D) {
         this.placeTag();
     }
 
-    setPosition(top, left) : void {
-        this.clear();
-        this.top = top;
-        this.left = left;
- 
-         this.placeTag();
-     }
- 
-
     placeTag() {
         this.context.fillStyle = 'rgba(0,0,0,0.0)';
-        this.context.fillRect(this.left,this.top,this.width, this.height);
-        this.context.drawImage(this.paintableCanvas.canvas, this.left, this.top);
-    };
-
-    setImage(image: PaintableCanvas) {
-        this.paintableCanvas = image;
+        this.context.fillRect(this.elmLeft,this.elmTop,this.paintableCanvas.width, this.paintableCanvas.height);
+        this.context.drawImage(this.paintableCanvas.canvas, this.elmLeft, this.elmTop);
     };
 
     clear() : void {
-        this.context.clearRect(this.left, this.top, this.width, this.height);
+        this.context.clearRect(this.elmLeft, this.elmTop, this.paintableCanvas.width, this.paintableCanvas.height);
     }
 
 }
