3562147da16dd9ca46289614d6680a28c634a19c
marcus hinz <marcus.fj.hinz@gmail.com>
2017-09-27 07:56:53 +0200

    IMPLEMENTED * save nickname / avater  id as preferences

--- frontend/src/app/gamestart/gamestart.component.ts
+++ frontend/src/app/gamestart/gamestart.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input, OnInit, OnDestroy, Output } from '@angular/core';
+import { Component, Input, OnInit, OnDestroy, Output, AfterViewInit } from '@angular/core';
 import { PlayerDataService } from "../services/player-data.service";
+import { PreferenceService } from "../services/preference.service";
 import { GameService } from "../services/game.service";
 import { WebsocketService } from "../services/websocket.service";
 
@@ -9,10 +10,42 @@ import { WebsocketService } from "../services/websocket.service";
   styleUrls: ['./gamestart.component.scss']
 })
 
-export class GamestartComponent {
-    playerName: string;
+export class GamestartComponent implements AfterViewInit, OnInit {
+    private playerName: string;
+
+    private readonly PLAYER_NAME : string = 'player-name';
+    private readonly AVATAR_ID : string = 'avatar-id';
+    private playerNameFromPrefs: string = this.preferenceService.getValue(this.PLAYER_NAME);
+    private avatarIdFromPrefs: string = this.preferenceService.getValue(this.AVATAR_ID);
+
     
-    constructor(public gameService: GameService, public playerDataService: PlayerDataService) {}
+    ngOnInit(): void {
+        if(this.playerNameFromPrefs){
+            this.playerName = this.playerNameFromPrefs;
+        }
+        if(this.avatarIdFromPrefs){
+            const id = Number.parseInt(this.avatarIdFromPrefs);
+            this.playerDataService.setPlayerAvatarId(id);
+        }
+    }
+
+    ngAfterViewInit(): void {
+        //OMG! a bug known as feature!
+        //to wrap the statement in setTimeOut only supresses a exception thrown in dev mode
+        //in prod mode the exception is not thrown
+        //plain and seriously crazy!!! 
+        window.setTimeout(() =>{
+            if(this.avatarIdFromPrefs){
+                const selected = document.getElementById('avatar-' + this.avatarIdFromPrefs);
+                selected['checked'] = 'checked';
+            }
+        },0);    
+    }
+        
+    constructor(private gameService: GameService, 
+        private playerDataService: PlayerDataService, 
+        private preferenceService: PreferenceService ) {}
+
 
     private manageGame(): void {
 
@@ -26,6 +59,16 @@ export class GamestartComponent {
     private beginGame() : void{
         this.playerDataService.setPlayerName(this.playerName);
         this.gameService.startGame();
+    }
+
+    private safeToPreferences() : void{
+        this.preferenceService.setValue(this.PLAYER_NAME, this.playerName);
+        this.preferenceService.setValue(this.AVATAR_ID, this.playerDataService.getPlayerAvatarId().toString());
+        
+
+    }
+    private removeFromPreferences() :void{
+
     }
 
 }
