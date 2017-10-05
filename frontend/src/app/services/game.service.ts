9c120f5abb0067ee11b55970823d3db876202896
marcus hinz <marcus.fj.hinz@gmail.com>
2017-10-05 08:36:41 +0200

    IMPLEMENTED * implement onDestroy

--- frontend/src/app/services/game.service.ts
+++ frontend/src/app/services/game.service.ts
@@ -1,21 +1,24 @@
-import { HighscoreComponent } from '../highscore/highscore.component';
+import { Injectable, OnDestroy } from '@angular/core';
 import { Subject, Subscription } from 'rxjs/Rx';
-import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 
-import { Game, GameState } from '../battlefield/game';
 import { WebsocketService } from '../services/websocket.service';
 import { PlayerDataService } from '../services/player-data.service';
+
+import { Game, GameState } from '../battlefield/game';
 import { Player } from '../models';
 
 @Injectable()
-export class GameService {
+export class GameService implements OnDestroy {
     private game: Game;
-    private highScoreSubject: Subject<Player[]> = new Subject<Player[]>();
+    private playerSubject: Subject<Player[]> = new Subject<Player[]>();
     private gameStateSubject: Subject<GameState> = new Subject<GameState>();
     private stateSubscription: Subscription;
 
-    constructor(private websocketService: WebsocketService, private playerDataService: PlayerDataService) { }
+    constructor(
+        private websocketService: WebsocketService
+        ,private playerDataService: PlayerDataService
+    ) { }
 
     public startGame(): void {
        
@@ -23,17 +26,17 @@ export class GameService {
             this.game = new Game(this.websocketService, this.playerDataService);
             this.game.start();
             this.stateSubscription = this.websocketService.getState().subscribe((state) => {
-                this.highScoreSubject.next(state.players);
-                this.gameStateSubject.next(this.getGameStateFoeService());
+                this.playerSubject.next(state.players);
+                this.gameStateSubject.next(this.getGameStateForService());
             });
         } else {
             //case of a restart --> reuse the old insstance
             this.game.resetGame();
         }
-        this.gameStateSubject.next(this.getGameStateFoeService());
+        this.gameStateSubject.next(this.getGameStateForService());
     }
 
-    public destroy(): void {
+    ngOnDestroy(): void {
         this.game.destroyGame();
         this.stateSubscription.unsubscribe();
         this.websocketService.disconnect();
@@ -44,10 +47,10 @@ export class GameService {
     }
 
     public getPlayers(): Observable<Player[]> {
-        return this.highScoreSubject.asObservable();
+        return this.playerSubject.asObservable();
     }
 
-    private getGameStateFoeService(){
+    private getGameStateForService(){
         if(!this.game) return GameState.gameOff;
         return this.game.getGameState();
     }
